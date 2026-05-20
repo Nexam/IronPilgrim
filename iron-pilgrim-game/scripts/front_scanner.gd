01625c55481f3a657d999a793b1036ed6df83ad0
Nexam <inexam@gmail.com>
2026-05-19 17:13:17 -0700

    Scan material fix

--- iron-pilgrim-game/scripts/front_scanner.gd
+++ iron-pilgrim-game/scripts/front_scanner.gd
@@ -1,43 +1,43 @@
 extends Node
 
-const outline_material:Material = preload("uid://xevk2k2pdnvn")
+const outline_material: Material = preload("uid://xevk2k2pdnvn")
+const SCAN_OVERLAY:Material = preload("uid://c5rld2qkdoes4")
 
-@export var mech:MechMk3
-var scan_target:Node3D
-var prev_ouline:Material
+@export var mech: MechMk3
 
-func _physics_process(delta: float) -> void:
+var scan_target: Node3D
+var highlighted_meshes: Array[MeshInstance3D] = []
+
+
+func _physics_process(_delta: float) -> void:
+	var new_target: Node3D = null
+
 	if mech.scan_raycast.is_colliding():
-		var obj = mech.scan_raycast.get_collider()
-		if obj is Node3D:
-			if obj != scan_target:
-				if scan_target:
-					_highlight_scannable(false)
-				scan_target = obj as Node3D
-				_highlight_scannable(true)
-	else:
+		var obj := mech.scan_raycast.get_collider()
+		if obj is Node3D and obj.is_in_group("scannable"):
+			new_target = obj as Node3D
+
+	if new_target != scan_target:
+		_clear_highlight()
+		scan_target = new_target
+
 		if scan_target:
-			_highlight_scannable(false)
-		scan_target = null
-		
-func _highlight_scannable(highlight:bool):
-	print( "hightlight (%s): %s "%[highlight, scan_target])
-	for c in scan_target.get_children():
-		if c is MeshInstance3D:
-			var mi = c as MeshInstance3D
-			var mat = mi.get_surface_override_material(0)
-			if not mat:
-				continue
-			if highlight:
-				if mat.next_pass:
-					prev_ouline = mat.next_pass
-				mat.next_pass = outline_material
-			else:
-				if not mat:
-					continue
-				if prev_ouline:
-					mat.next_pass = prev_ouline
-				else:
-					mat.next_pass = null
-				prev_ouline = null
-			
+			_apply_highlight(scan_target)
+
+
+func _apply_highlight(root: Node3D) -> void:
+	print(root)
+	highlighted_meshes.clear()
+
+	for child in root.find_children("*", "MeshInstance3D", true, false):
+		var mi := child as MeshInstance3D
+		mi.material_override = SCAN_OVERLAY
+		highlighted_meshes.append(mi)
+
+
+func _clear_highlight() -> void:
+	for mi in highlighted_meshes:
+		if is_instance_valid(mi):
+			mi.material_override = null
+
+	highlighted_meshes.clear()
